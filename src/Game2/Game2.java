557d7e16d7633612a709437591dc5f5775d8a4af
IvanGer212 <Comradeivan88@gmail.com>
2021-02-05 11:27:52 +0300

    Add method for check 2 words and write result in console

--- src/Game2/Game2.java
+++ src/Game2/Game2.java
@@ -1,6 +1,7 @@
 package Game2;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Game2 {
     public static void main(String[] args) {
@@ -8,12 +9,39 @@ public class Game2 {
                 "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
                 "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
         chooseTheWord(words);
-        System.out.println(chooseTheWord(words));
+        String compWord = new String();
+        compWord = chooseTheWord(words);
+        System.out.println(compWord);
+        checkTheWord(compWord,readUserWord());
+
 
     }
     public static String chooseTheWord (String[] words){
         int max = words.length-1;
         Random random = new Random();
         return words[random.nextInt(max)];
+    }
+
+    public static String readUserWord(){
+        System.out.println("Введите слово ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static void checkTheWord (String randWord, String putWord){
+        int n = randWord.length()-1;
+        for (int i = 0; i<15; i++){
+            if (i<n){
+                if (putWord.charAt(i)==randWord.charAt(i)){
+                    System.out.print(putWord.charAt(i));
+                }else System.out.print("#");
+            }
+            else {
+                System.out.print("#");
+            }
+            }
+        System.out.println();
+        System.out.println("Загадано слово: "+randWord);
+
     }
 }
